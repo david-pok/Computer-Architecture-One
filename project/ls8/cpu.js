1033f06c1275e2619e36c7a874f5335f2fecca69
David Pok <david.pok@gmail.com>
2018-06-14 14:49:47 -0700

    unsuccessfully adds RET and CALL

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -5,6 +5,13 @@ const LDI = 0b10011001;
 const PRN = 0b01000011; // Print numeric register
 const HLT = 0b00000001;
 const MUL = 0b10101010;
+const PUSH = 0b01001101;
+const POP = 0b01001100;
+const CALL = 0b01001000;
+const RET = 0b00001001;
+
+const SP = 7;
+
 /**
  * Class for simulating a simple Computer (CPU & memory)
  */
@@ -88,6 +95,12 @@ class CPU {
         // Execute the instruction. Perform the actions for the instruction as
         // outlined in the LS-8 spec.
 
+            const _push = value => {
+                this.reg[SP]--;
+                this.ram.write(this.reg[SP], value);
+            }
+            //^^manually set push down below in push case
+
         // !!! IMPLEMENT ME
             switch(IR){ 
 
@@ -95,6 +108,7 @@ class CPU {
                 // Set the value in a register
                 this.reg[operandA] = operandB;
                 //this.PC += 3; // Next instruction
+                console.log(`in reg operandA write operandB`)
                 break;
 
                 case MUL:
@@ -109,6 +123,27 @@ class CPU {
                this.stopClock();
                //this.PC += 1;
                break;
+
+                case PUSH:
+                if (this.reg[SP] === 0) {
+                    this.reg[SP] = 0xf4
+                }
+                _push(this.reg[operandA]);
+                //need to set _push above for this to work
+                //so you dont have to repeat yourself
+                //or use this below 
+                // this.reg[SP]--;
+                // this.ram.write(this.reg[SP], this.reg[operandA]);
+                break;
+
+                case CALL:
+                this._push(this.pc+2);
+                break;
+                
+                case POP:
+                this.reg[operandA] = this.ram.read(this.reg[SP]);
+                this.reg[SP]++;
+                break;
 
                default: 
                break;
